af0bc81e05f1edfcc26f84f1cf0cc85da497fcf2
Bohdan Kostenko <bohdan.kostenko2020@gmail.com>
2021-04-06 19:41:38 +0300

    Home work 3

--- lesson3/hw3.py
+++ lesson3/hw3.py
@@ -1,19 +1,18 @@
 def main():
     n = int(input("Enter the range of numbers: "))
+
+    def fibonacci(n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fibonacci(n-2) + fibonacci(n-1)
     result = []
     start = 0
     for n in range(start, n+1):
         result.append(fibonacci(n))
     print(result)
-
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-2) + fibonacci(n-1)
 
 
 if __name__ == "__main__":
